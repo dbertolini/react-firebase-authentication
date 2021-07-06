4538c4043dfe5502684836a294fd0a38e264ad8d
Diego Bertolini <bertolini.diego@gmail.com>
2021-07-06 01:52:53 -0300

    Display name and email verification added.

--- src/App.js
+++ src/App.js
@@ -6,43 +6,55 @@ const firebaseInstance = getFirebase();
 
 function App() {
 
+  const [fullname, setFullname] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
-  const signIn = async (event) => {
-    event.preventDefault();
-
+  const signInEmail = async (event) => {
     try {
       if (firebaseInstance) {
-        const user = await firebaseInstance
-          .auth()
-          .signInWithEmailAndPassword(email, password);
-        console.log("user", user);
-        alert("Welcome back!");
+        const res = await firebaseInstance.auth().signInWithEmailAndPassword(email, password);
+
+        console.log("Response: ", res)
+
+        if(res.user.emailVerified) {
+          alert("Welcome back!")
+        }
+        else {
+          alert("Please, verify your email account!")
+        }
       }
     } catch (error) {
-      console.log("error", error);
+      console.log("error", error)
+      alert(error.message)
     }
-  };
+  }
 
-  const signUp = async(event) => {
-    event.preventDefault();
-
+  const signUpEmail = async(event) => {
     try {
       if (firebaseInstance) {
-        const user = await firebaseInstance.auth().createUserWithEmailAndPassword(email, password)
-        console.log("user", user)
-        alert(`Welcome ${email}!`);
+        const res = await firebaseInstance.auth().createUserWithEmailAndPassword(email, password)
+        console.log("Response: ", res)
+        res.user.updateProfile({
+          displayName: fullname
+        })
+        res.user.sendEmailVerification();
+        firebaseInstance.auth().signOut();
+        alert('New account created! Please, verify your email to activate the account!');
       }
     } catch (error) {
       console.log("error", error);
       alert(error.message);
     }
-
   };
 
   return (
-    <div className="App">
+    <div>
+      <h1>Email Sign In</h1>
+      <div>
+        <label htmlFor="txtFullname">Fullname:</label>
+        <input onChange={(e)=>setFullname(e.target.value)} type="text" id="txtFullname" placeholder="Fullname"></input>
+      </div>
       <div>
         <label htmlFor="txtEmail">Email:</label>
         <input onChange={(e)=>setEmail(e.target.value)} type="text" id="txtEmail" placeholder="Email"></input>
@@ -52,10 +64,10 @@ function App() {
         <input onChange={(e)=>setPassword(e.target.value)} type="password" id="txtPassword" placeholder="Password"></input>
       </div>
       <div>
-        <button onClick={(e) => signIn(e)}>Sign In</button>
+        <button onClick={(e) => signInEmail(e)}>Sign In</button>
       </div>
       <div>
-        <button onClick={(e) => signUp(e)}>Sign Up</button>
+        <button onClick={(e) => signUpEmail(e)}>Sign Up</button>
       </div>
     </div>
   );
